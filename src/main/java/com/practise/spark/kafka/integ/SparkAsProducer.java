7120d8a3e9ef0173392111b5e497a83850d19cd9
mailtonskumar <mailtonskumar@gmail.com>
2021-10-01 11:11:10 +0000

    spark kafka as a producer

---
+++ src/main/java/com/practise/spark/kafka/integ/SparkAsProducer.java
@@ -0,0 +1,26 @@
+package com.practise.spark.kafka.integ;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.streaming.StreamingQueryException;
+
+public class SparkAsProducer {
+
+    public static void main(String ...args) throws StreamingQueryException {
+        SparkSession spark = SparkSession.builder()
+                .master("local[*]")
+                .appName("kafka integration example")
+                .getOrCreate();
+        spark.sparkContext().setLogLevel("ERROR");
+        Dataset<Row> df = spark
+                .readStream()
+                .format("kafka")
+                .option("kafka.bootstrap.servers", "localhost:9092")
+                .option("subscribe", "topic4")
+                .load();
+        Dataset<Row> ds =  df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)");
+        ds.writeStream().format("kafka").option("kafka.bootstrap.servers", "localhost:9092")
+                .option("topic", "topic6").option("checkpointLocation","file:///home/labuser/chk").start().awaitTermination();
+    }
+}
